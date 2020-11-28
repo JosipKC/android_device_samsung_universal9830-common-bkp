4f74f234125f6837ac1740fc18dbf575d7876ad3
Josip <josip.kelecic@sartura.hr>
2020-11-28 18:25:49 +0100

    KERNEL_CMDLINE: leave only selinux permissive
    Change-Id: Id8661536ec7ad99d205c28e7dceed6baf2cf4bb9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -34,7 +34,6 @@ TARGET_KERNEL_CLANG_COMPILE := true
 # Image
 BOARD_BOOT_HEADER_VERSION := 2
 BOARD_KERNEL_BASE := 0x10000000
-BOARD_KERNEL_CMDLINE := console=null androidboot.hardware=exynos990 androidboot.memcg=1 lpm_levels.sleep_disabled=1 video=vfb:640x400,bpp=32,memsize=3072000 msm_rtb.filter=0x237 service_locator.enable=1 androidboot.usbcontroller=a600000.dwc3 swiotlb=2048 printk.devkmsg=on firmware_class.path=/vendor/firmware_mnt/image
 BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive androidboot.selinux=permissive
 BOARD_KERNEL_IMAGE_NAME := Image
 BOARD_KERNEL_OFFSET := 0x00008000
